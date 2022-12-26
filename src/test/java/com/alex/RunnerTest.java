eb42db848609d277ee7166c8351c6a645bbcccce
Bulbash <zverovik@yahoo.com>
2022-12-26 22:42:32 +0300

    task1 array manipulation (added Maven)

---
+++ src/test/java/com/alex/RunnerTest.java
@@ -0,0 +1,14 @@
+package com.alex;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RunnerTest {
+
+    @org.junit.jupiter.api.Test
+    void countOfMassive() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void countOfMassiveToStream() {
+    }
+}
