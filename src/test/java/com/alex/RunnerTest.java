a2623961bc8d18c2b3a93f3c4a3872b2d58fc693
Bulbash <zverovik@yahoo.com>
2022-12-28 13:05:08 +0300

    task1 array manipulation (first attempt make tests)

--- src/test/java/com/alex/RunnerTest.java
+++ src/test/java/com/alex/RunnerTest.java
@@ -1,13 +1,17 @@
 package com.alex;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 
 class RunnerTest {
+    int[] mass1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
 
     @Test
     void countOfMassive() {
+        int[] massive = Runner.countOfMassive(mass1);
+        Assertions.assertArrayEquals(new int[]{10,-65}, massive);
     }
 
     @Test
