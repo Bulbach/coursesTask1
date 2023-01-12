0a1440c83bd8819deb614bb346af9516dac2bee2
Bulbash <zverovik@yahoo.com>
2023-01-12 13:06:05 +0300

    task2 создание контроллера

---
+++ task2/src/main/java/com/example/task2/controllers/CargoController.java
@@ -0,0 +1,31 @@
+package com.example.task2.controllers;
+
+import com.example.task2.model.Cargo;
+import com.example.task2.serviceses.typeOfConstruct.CargoIfConstruct;
+import com.example.task2.serviceses.typeOfConstruct.CargoMapConstruct;
+import com.example.task2.serviceses.typeOfConstruct.CargoSwitchConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+public class CargoController {
+    @Autowired
+    private CargoIfConstruct cargoIfConstruct;
+    @Autowired
+    private CargoSwitchConstruct cargoSwitchConstruct;
+    @Autowired
+    private CargoMapConstruct cargoMapConstruct;
+
+
+    @GetMapping(value = "/")
+    public String hello() {
+        return "Hello";
+    }
+
+    @PostMapping("/cargo")
+    public Cargo treatment(@RequestBody Cargo cargo){
+        cargoMapConstruct.chooseOperation(cargo);
+        return cargo;
+    }
+}
